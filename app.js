8c253c8ed117a6b224fb890fa32eaddc99b29b6a
Eleonora <eleonora1weber@gmail.com>
2021-12-08 16:18:54 +0100

    added some middleware

--- app.js
+++ app.js
@@ -7,9 +7,47 @@ const items = [
   { id: 2, name: 'item2' },
 ];
 
-app.get('/myroute', (req, res) => {
+const middleware1 = (req, res, next) => {
+  console.log("doing stuff in middleware 1");
+  next();
+}
+
+const middleware2 = (req, res, next) => {
+  console.log('doing stuff in middleware 2');
+  next();
+}
+
+const extractUserName = (req, res, next) => {
+  if (req.query.name) {
+    req.userName = req.query.name;
+  } else {
+    res.status(400).send('You have to specify a "name" query parameter call this')
+  }
+}
+const handleError = (err, req, res, next) => {
+  console.error(err);
+}
+//app.use(middleware1);
+//app.use(middleware2);
+
+
+app.get('/myroute', middleware2, (req, res) => {
   console.log('handling /myroute');
   res.send(items);
 });
+
+app.get('/hello', extractUserName, (req, res) => {
+  console.log("handling /hello");
+  console.log(`Hello, ${req.userName}`)
+  res.send(`Hello, ${req.userName}`);
+});
+
+app.get('/syncError', (req, res) => {
+  setTimeout(() => {
+    throw new Error("Boom! async")
+  }, 1000);
+})
+
+app.use(handleError);
 
 app.listen(5000, () => console.log('server listening on port 5000'));
